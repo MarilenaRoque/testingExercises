f772efffa265e7ee7aedb920719ee1bba3e1dd7e
MarilenaRoque <marilenas@id.uff.br>
2020-11-13 14:35:39 -0300

    Build the tests and function add for calculator
    Create the files and tests for all the functionalitites of the calculator
    Create a file to store the logic
    Build a function to respond to add functionality

---
+++ calculator.js
@@ -0,0 +1,25 @@
+const sum = (total, item) => {
+    total + item;
+}
+
+const calculator = (() => {
+    if 
+    const add = (...args) => {
+        let result = args.reduce(function(total, item) {
+            return total + item;
+        },0);
+        return result;
+    }
+
+    const subtract = (a, b) => {
+        
+    }
+
+
+
+    return {add};
+
+})();
+
+
+module.exports = calculator;
